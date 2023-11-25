e222aee6932c7cec845ee3f35bbaac0ba97019c4
ramafahad <52184019+ramafahad@users.noreply.github.com>
2023-11-25 19:31:17 +0300

    kids pics

--- displayChild.js
+++ displayChild.js
@@ -1,15 +1,43 @@
 function displayChildName() {
-    var childNames = JSON.parse(localStorage.getItem('names') || '[]');
+  if (JSON.parse(localStorage.getItem('names')==null)){
     var kidsContainer = document.querySelector('.kidscontainer');
-  
-    childNames.forEach(function(name) {
-      var kidInfoElement = document.createElement('div');
-      kidInfoElement.classList.add('kidinfo');
-  
-      var nameElement = document.createElement('h3');
-      nameElement.textContent = name;
-  
-      kidInfoElement.appendChild(nameElement);
-      kidsContainer.appendChild(kidInfoElement);
-    });
+    const randName=["Taleen Fahad", "Rayan Fahad"]
+    const imgsrc=["images/daughter.png", "images/son.png" ]
+    for (let i=0; i<2; i++){
+    var kidInfoElement = document.createElement('div');
+    kidInfoElement.classList.add('kidinfo');
+
+    var picElement = document.createElement('img');
+    picElement.src = imgsrc[i];
+    picElement.alt = "kid image" ;
+
+    var nameElement = document.createElement('h3');
+    nameElement.textContent = randName[i];
+
+    kidInfoElement.appendChild(picElement);
+    kidInfoElement.appendChild(nameElement);
+    kidsContainer.appendChild(kidInfoElement);
+}
+
   }
+  else{
+  var childNames = JSON.parse(localStorage.getItem('names') || '[]');
+  var kidsContainer = document.querySelector('.kidscontainer');
+  childNames.forEach(function(name, index) {
+    var kidInfoElement = document.createElement('div');
+    kidInfoElement.classList.add('kidinfo');
+
+
+    var imgs = JSON.parse(localStorage.getItem('images') || '[]');
+    var picElement = document.createElement('img');
+    picElement.src =imgs[index];
+
+    var nameElement = document.createElement('h3');
+    nameElement.textContent = name;
+
+    kidInfoElement.appendChild(picElement);
+    kidInfoElement.appendChild(nameElement);
+    kidsContainer.appendChild(kidInfoElement);
+  });
+}
+}
