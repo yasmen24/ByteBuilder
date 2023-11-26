d0306a4860dd3aaa15a9a83d07e9408d11c2ae16
yasmen24 <yasmensaleh24@gmail.com>
2023-11-26 22:08:55 +0300

    dance

--- AdminJava.js
+++ AdminJava.js
@@ -1,31 +1,35 @@
 
 
 onload=function(){
-   
-    let holder=document.getElementById('currentdate');
-    let current =document.createElement('div');
-    let todayDate =document.createElement('p');
 
-  
 
-    
-    let date=new Date();
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    
-    
-     ///get important info about date
-     let day=date.getDay();
-    let month=date.getMonth();//it start count from 0
-    let datee=date.getDate();
-    let year=date.getFullYear();
-    
-    ///set it in html element
-    let content=document.createTextNode(`${days[day]} , ${datee}  ${months[month]} ${year}`);
-    todayDate.appendChild(content);
-    current.appendChild(todayDate);
-    holder.appendChild(current);
-    current.style.cssText=
+    function updateSundayDate() {
+        let holder=document.getElementById('currentdate');
+        let current =document.createElement('div');
+        let todayDate =document.createElement('p');
+
+
+
+
+
+        const today = new Date();
+        const dayOfWeek = today.getDay(); 
+      
+        const daysUntilSunday = dayOfWeek === 0 ? 0 : 7 - dayOfWeek;
+      
+        const currentSunday = new Date(today.getTime() - dayOfWeek * 24 * 60 * 60 * 1000);
+        const nextSunday = new Date(today.getTime() + daysUntilSunday * 24 * 60 * 60 * 1000);
+      
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedCurrentSunday = currentSunday.toLocaleDateString('en-US', options);
+        const formattedNextSunday = nextSunday.toLocaleDateString('en-US', options);
+      
+        
+        let content=document.createTextNode(` ${formattedCurrentSunday}`);
+        todayDate.appendChild(content);
+        current.appendChild(todayDate);
+        holder.appendChild(current);
+        current.style.cssText=
     `
     background: #eef2f3;
     padding=3px;
@@ -33,10 +37,14 @@ onload=function(){
     border-top:1px dotted gray;
     border-bottom:1px dotted gray;
     text-align:center;    
-    
-    
-    
     `
+
+      }
+      
+      // Call the function to check and display the date
+      updateSundayDate();
+
+
 
     
     
@@ -52,92 +60,172 @@ let Allstudent=['zaid Abdullah','Julien Muhammad', 'saud Muhammad','Remaz Sliman
 
 
 ///courses students 
-let AICourse=['Reema Abdulrahman','Nora Mansour','Taleen Alshebel','Rayan Alshebel','Dana Mansour','Hassan Saleh','Lana Slimane','Bassam Ahmad']
-let  arduinoCourse=['Nora Mansour','zaid Abdullah','Bassam Ahmad','Remaz Slimane','Dana Mansour','Hassan Saleh','Abdullah Saleh']
 
-let  scratshCourse=['Sara Muhammad','Julien Muhammad', 'Shatha Saleh' ,'Talal Muhammad' ,'Moaz Muhammad','Hesham Slimane','Lana Slimane']
+let allcources=['AICourse' ,'arduinoCourse','scratshCourse']
+
+let AICourse=['Taleen Alshebel','Hassan Saleh','Dana Mansour','Rayan Alshebel','Lana Slimane','Bassam Ahmad','Nora Mansour']
+
+let  arduinoCourse=['Nora Mansour','Bassam Ahmad','Remaz Slimane','zaid Abdullah','Dana Mansour','Hassan Saleh','Sara Muhammad']
+
+let  scratshCourse=['Sara Muhammad','Talal Muhammad','lara Muhammad','Hassan Saleh','Shatha Saleh','Hesham Slimane','Lana Slimane','Abdullah Saleh']
+
+
+
 ///////////////////////////////////////
  
+
+
+
+
+
+
 ///student of the given class
 
+let  showMore;
+var closebtn;
+var index;
+showMore=document.querySelectorAll('.btn');
+ for(  i=0;i<showMore.length;i++ ){
+ showMore[i].addEventListener('click',courses);
 
- let  showMore=document.querySelectorAll('.btn');
- for( i=0;i<showMore.length;i++ )
- showMore[i].addEventListener('click',courses)
-
+}
  var course;
+ var list;
+ var closeBtn;
+var arrayC;
+ function courses (z){
 
-
- function courses (){
     course=`${this.name}Course`;
-    console.log(course);
 
+    
    switch(course){
+
+   case 'AICourse': 
+   index=0;
    
-   case 'AICourse': studentwindoe(AICourse);
+   list=document.getElementsByClassName('listS')[0];
+   arrayC=AICourse;
+
+   studentwindoe(arrayC);
+
    break;
-   case 'arduinoCourse':studentwindoe(arduinoCourse) ;break;
-   case 'scratshCourse':studentwindoe(scratshCourse);break;
+
+
+   case 'arduinoCourse':
+    index=1;
+
+    list=document.getElementsByClassName('listS')[1];
+    arrayC=arduinoCourse;
+    studentwindoe(arrayC) ;
+
+
+    break;
+
+   case 'scratshCourse':
+    index=2;
+
+    list=document.getElementsByClassName('listS')[3];
+    arrayC=scratshCourse;
+    studentwindoe(arrayC);
+
+    break;
    
    }
    
    }
 
-
-
-
-
-let pop=document.getElementsByClassName('popup')[0];
 let students;
-let tex;
-let popupWindow=document.getElementById("show");
-let closeBtn=document.getElementById("ok");
-let list=document.createElement('ul');
 
-let studentwindoe=function (x){
+
+
+//show less button
+
+let studentwindoe=function (arrayC){
     list.innerHTML='';
-    for(let i=0;i<x.length;i++)
+   
+
+    for(let j=0;j<arrayC.length;j++)
     {
         students=document.createElement('li');
-        tex=document.createTextNode(x[i]);
+        if(j%2==0){
+            students.classList.add('girl');
+        }
+       
+        else {students.classList.add('boy');}
+        
+        tex=document.createTextNode(arrayC[j]);
         students.appendChild(tex);
        list.appendChild(students)
     }
-    
-    pop.style.visibility='visible';
-    popupWindow.appendChild(list);
-    
-    }
-
-    
-    closeBtn.onclick=function(){
-    
-        pop.style.visibility='hidden';
-        list.innerHTML='';
-
+    closse();
     }
     
+    var closebtn;
+   function closse(){
     
     
+   switch(index){
+
+    case 0: 
+    closebtn=document.getElementsByClassName('ok')[0];
+    showMore=document.querySelectorAll('.btn')[0];
+    closebtn.addEventListener('click',showless);
+
+   // closebtn.addEventListener('click',removeStudent(index,arrayC))
+
+
+    break;
+    case 1:
+        closebtn=document.getElementsByClassName('ok')[1];
+        showMore=document.querySelectorAll('.btn')[1];
+        closebtn.addEventListener('click',showless);
+
+       // closebtn.addEventListener('click',removeStudent(index,arrayC))
+
+
+     break;
+    case 2:
+        closebtn=document.getElementsByClassName('ok')[2];
+        showMore=document.querySelectorAll('.btn')[2];
+        closebtn.addEventListener('click',showless);
+
+     break;
+    
+    }
+    closebtn.style.cssText=`    visibility: visible; `
+    showMore.style.cssText=`    visibility: hidden; `
+
+    
+
+
+
+   } 
+   
+    function showless(){
+
+    list.innerHTML='';
+    for(l=0;l<5;l++)
+
+       {
+        students=document.createElement('li');
+        if(l%2==0){
+            students.classList.add('girl');
+        }
+       
+        else {students.classList.add('boy');}
+        
+        tex=document.createTextNode(arrayC[l]);
+        students.appendChild(tex);
+       list.appendChild(students)
+    }
+    closebtn.style.cssText=`    visibility:hidden ; `
+    showMore.style.cssText=`    visibility: visible; `
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   }
 
 
 
